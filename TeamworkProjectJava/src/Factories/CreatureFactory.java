b52958885968824c1528e6451ef22b9915419f46
viktorpts <viktor.pts@abv.bg>
2016-04-15 13:56:21 +0300

    fixed references to renamed class Abilities.Attack to Abilities.MeleeAttack

--- TeamworkProjectJava/src/Factories/CreatureFactory.java
+++ TeamworkProjectJava/src/Factories/CreatureFactory.java
@@ -9,26 +9,27 @@ import Interfaces.IEnemyProducible;
 import Interfaces.IPlayerProducible;
 import Models.Enemy;
 import Models.Entity;
-import Abilities.Attack;
+import Abilities.MeleeAttack;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class CreatureFactory implements IEnemyProducible, IPlayerProducible {
 
-    static Enemy GiantRat = createEnemy(EnemyTypes.GIANT_RAT,0,0,0);
-    static Enemy Slime = createEnemy(EnemyTypes.SLIME,0,0,0);
-    static Enemy Zombie = createEnemy(EnemyTypes.ZOMBIE,0,0,0);
-    static HashMap<EnemyTypes,Enemy> enemies = new HashMap<EnemyTypes,Enemy>(); //HashMap with all enemies
+    static Enemy GiantRat = createEnemy(EnemyTypes.GIANT_RAT, 0, 0, 0);
+    static Enemy Slime = createEnemy(EnemyTypes.SLIME, 0, 0, 0);
+    static Enemy Zombie = createEnemy(EnemyTypes.ZOMBIE, 0, 0, 0);
+    static HashMap<EnemyTypes, Enemy> enemies = new HashMap<EnemyTypes, Enemy>(); //HashMap with all enemies
 
-    public static void init(){ //Initialize HashMap with all enemies
-        enemies.put(EnemyTypes.GIANT_RAT,GiantRat);
-        enemies.put(EnemyTypes.SLIME,Slime);
-        enemies.put(EnemyTypes.ZOMBIE,Zombie);
+    public static void init() { //Initialize HashMap with all enemies
+        enemies.put(EnemyTypes.GIANT_RAT, GiantRat);
+        enemies.put(EnemyTypes.SLIME, Slime);
+        enemies.put(EnemyTypes.ZOMBIE, Zombie);
     }
-    public static Enemy createEnemy(EnemyTypes type, double x, double y, double direction)
-    {
+
+    public static Enemy createEnemy(EnemyTypes type, double x, double y, double direction) {
         Enemy thisEnemy = new Enemy(
-                type.getAnimation(),x,y ,direction,
+                type.getAnimation(), x, y, direction,
                 type.getHealthPoints(),
                 type.getAttackPoints(),
                 type.getArmorValue(),
@@ -37,16 +38,16 @@ public class CreatureFactory implements IEnemyProducible, IPlayerProducible {
                 type.getMaxSpeed(),
                 type.getMaxAcceleration()
         );
-        thisEnemy.addAbility(Abilities.ATTACKPRIMARY, new Attack(thisEnemy, type.getAttackPoints(),type.getRadius()));
+        thisEnemy.addAbility(Abilities.ATTACKPRIMARY, new MeleeAttack(thisEnemy, type.getAttackPoints(), type.getRadius()));
         thisEnemy.addBrain(new Roam(thisEnemy, 0.3));
         thisEnemy.addBrain(new Gank(thisEnemy, 3));
         return thisEnemy;
     }
-    public static Enemy spawnEnemy(EnemyTypes type, double x, double y, double direction)
-    {
-        Enemy enemyToSpawn = enemies.getOrDefault(type,null);
-        enemyToSpawn.setPos(x,y);
+
+    public static Enemy spawnEnemy(EnemyTypes type, double x, double y, double direction) {
+        Enemy enemyToSpawn = enemies.getOrDefault(type, null);
+        enemyToSpawn.setPos(x, y);
         enemyToSpawn.setDirection(direction);
-            return enemyToSpawn;
-        }
+        return enemyToSpawn;
     }
+}
