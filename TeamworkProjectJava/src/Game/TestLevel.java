288dd9888a6fc4730fa5720ad7f5ef214725c5b0
viktorpts <viktor.pts@abv.bg>
2016-04-17 12:20:15 +0300

    pressing enter generates completely new dungeon each time

--- TeamworkProjectJava/src/Game/TestLevel.java
+++ TeamworkProjectJava/src/Game/TestLevel.java
@@ -19,9 +19,6 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class TestLevel  extends Application {
-    // TODO: place level gen and display code here; display is sensitive to grid size (recommended 4-8)
-    static ArrayList<Dungeon> dungeonList;
-    private DungeonMaker map;
 
     public static void main(String[] args) {
         // Add functionality to display generated map, like a foreach. Use the output of the following method:
@@ -43,8 +40,6 @@ public class TestLevel  extends Application {
         QuickView.gridSize = 5;
         QuickView.drawGrid(gc);
 
-        // First pass
-        map = new DungeonMaker();
         // Register event handler for key presses
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             public void handle(KeyEvent ke) {
@@ -53,20 +48,14 @@ public class TestLevel  extends Application {
                 gc.fillRect(0, 0, 800, 600);
                 QuickView.drawGrid(gc);
                 if (ke.getCode() == KeyCode.ENTER) {
-                    // Finalize generation, make rooms within boundaries
-
-                    //QuickView.renderDungeon(gc, map.getMaze(), true);
+                    // Initialize generator, make rooms within boundaries
+                    DungeonMaker map = new DungeonMaker();
                     gc.setFill(Color.WHITE);
                     map.getLevelTiles().stream().forEach(tile -> {
                         gc.setFill(tile.getTileType() == TileType.WALL ? Color.GREY : Color.WHITE);
                         gc.fillRect(tile.getX() * 2, tile.getY() * 2, 2, 2);
                     });
                 }
-//                else {
-//                    // Iterate dungeon
-//                    GenerateDungeon.sampleStep(dungeonList);
-//                    QuickView.renderDungeon(gc, dungeonList, false);
-//                }
             }
         });
 
